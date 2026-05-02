0790a239b300130b674986b7ef02da83107e5da2
Duncan <205854574+AmpVolume@users.noreply.github.com>
2026-05-02 11:42:07 -0400

    Implement inheritance with Animal, Dog, Cat, and Puppy classes

--- IBM/ IBM Java Developer Professional Certificate/Inheritance in Java/Main.java
+++ IBM/ IBM Java Developer Professional Certificate/Inheritance in Java/Main.java
@@ -1,1 +1,72 @@
+class Animal {
+    String name;
 
+    void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println(name + " says woof!");
+    }
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println(name + " says meow!");
+    }
+
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+class Puppy extends Dog {
+    void weep() {
+        System.out.println(name + " is weeping.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=== BASIC INHERITANCE ===");
+        Dog myDog = new Dog();
+        myDog.name = "Buddy";
+        myDog.eat();
+        myDog.bark();
+
+        System.out.println("\n=== MULTILEVEL INHERITANCE ===");
+        Puppy myPuppy = new Puppy();
+        myPuppy.name = "Max";
+        myPuppy.eat();
+        myPuppy.bark();
+        myPuppy.weep();
+
+        System.out.println("\n=== HIERARCHICAL INHERITANCE ===");
+        Cat myCat = new Cat();
+        myCat.name = "Whiskers";
+        myCat.eat();
+        myCat.meow();
+
+        System.out.println("\n=== POLYMORPHISM ===");
+        Animal a1 = new Animal();
+        Animal a2 = new Dog();
+        Animal a3 = new Cat();
+
+        a1.sound();
+        a2.sound();
+        a3.sound();
+    }
+}
