bb9137bec1457d9f167e9d8bb11f1e1c84e32d54
Duncan <205854574+AmpVolume@users.noreply.github.com>
2026-05-02 12:08:19 -0400

    Implement polymorphism with MathOperations and Animals

--- IBM/ IBM Java Developer Professional Certificate/Polymorphism in Java/Main.java
+++ IBM/ IBM Java Developer Professional Certificate/Polymorphism in Java/Main.java
@@ -1,1 +1,69 @@
+class MathOperations {
 
+    // Method 1: two ints
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method 2: three ints
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method 3: two doubles
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+
+// Runtime polymorphism classes
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+
+// MAIN CLASS (ENTRY POINT)
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== COMPILE-TIME POLYMORPHISM ===");
+
+        MathOperations math = new MathOperations();
+
+        System.out.println("Sum of 2 and 3: " + math.add(2, 3));
+        System.out.println("Sum of 2, 3, 4: " + math.add(2, 3, 4));
+        System.out.println("Sum of 2.5 and 3.5: " + math.add(2.5, 3.5));
+
+
+        System.out.println("\n=== RUNTIME POLYMORPHISM ===");
+
+        Animal a;
+
+        a = new Animal();
+        a.sound();
+
+        a = new Dog();
+        a.sound();
+
+        a = new Cat();
+        a.sound();
+    }
+}
