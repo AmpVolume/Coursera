2e0753c4e1872ffb7c1097d418dc17e03eb4c907
Duncan <205854574+AmpVolume@users.noreply.github.com>
2026-05-02 07:41:39 -0400

    Implement Dog, Cat, and Cow classes with sounds
    Added Dog, Cat, and Cow classes implementing the sound method.

--- IBM/ IBM Java Developer Professional Certificate/Lab: Implementing Polymorphism/Animal.java
+++ IBM/ IBM Java Developer Professional Certificate/Lab: Implementing Polymorphism/Animal.java
@@ -1,1 +1,32 @@
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
 
+    @Override
+    public String sound() {
+        return "Woof";
+    }
+}
+
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sound() {
+        return "Meow";
+    }
+}
+
+class Cow extends Animal {
+    public Cow(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sound() {
+        return "Moo";
+    }
+}
